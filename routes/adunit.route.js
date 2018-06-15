c3ce0893fb44d751c38b81d5a4e70cc5efa431f0
Elisa <lisablaga@gmail.com>
2018-06-15 14:21:06 +0200

    reverting back to previous version

--- routes/adunit.route.js
+++ routes/adunit.route.js
@@ -1,5 +1,6 @@
 // adunit.route.js
 
+
 const express = require('express');
 const app = express();
 const adUnitRoutes = express.Router();
@@ -9,55 +10,55 @@ let AdUnit = require('../models/AdUnit');
 
 // Defined store route
 adUnitRoutes.route('/add').post(function (req, res) {
-  let adUnit = new AdUnit(req.body);
-  adUnit.save()
-    .then(game => {
-    res.status(200).json({'adUnit': 'AdUnit in added successfully'});
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });
+    let adUnit = new AdUnit(req.body);
+    adUnit.save()
+        .then(game => {
+        res.status(200).json({'adUnit': 'AdUnit in added successfully'});
+})
+.catch(err => {
+        res.status(400).send("unable to save to database");
+});
 });
 
 // Defined get data(index or listing) route
 adUnitRoutes.route('/').get(function (req, res) {
     AdUnit.find(function (err, adUnits){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(adUnits);
-    }
-  });
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.json(adUnits);
+        }
+    });
 });
 
 // Defined edit route
 adUnitRoutes.route('/edit/:id').get(function (req, res) {
-  let id = req.params.id;
-  AdUnit.findById(id, function (err, adUnit){
-      res.json(adUnit);
-  });
+    let id = req.params.id;
+    AdUnit.findById(id, function (err, adUnit){
+        res.json(adUnit);
+    });
 });
 
 //  Defined update route
 adUnitRoutes.route('/update/:id').post(function (req, res) {
     AdUnit.findById(req.params.id, function(err, adUnit) {
-    if (!adUnit)
-      return next(new Error('Could not load Document'));
-    else {
-        adUnit.unit_price.push({
-          value: req.body.unit_price
-        });
+        if (!adUnit)
+            return next(new Error('Could not load Document'));
+        else {
+            adUnit.unit_price.push({
+                value: req.body.unit_price
+            });
 
-        console.log(req.body.unit_price);
-        adUnit.save().then(adUnit => {
-          res.json('Update complete');
-      })
-      .catch(err => {
-            res.status(400).send("unable to update the database");
-      });
-    }
-  });
+            console.log(req.body.unit_price);
+            adUnit.save().then(adUnit => {
+                res.json('Update complete');
+        })
+        .catch(err => {
+                res.status(400).send("unable to update the database");
+        });
+        }
+    });
 });
 
 // Defined delete | remove | destroy route
--- server.js
+++ server.js
@@ -6,27 +6,40 @@ const express = require('express'),
     morgan = require('morgan'),
     passport = require('passport'),
     config = require('./config/DB');
+//var io = require('socket.io')(server);
 
-    const app = express();
-    app.use(express.static(path.join(__dirname, './client/dist/project')));
+const app = express();
+app.use(express.static(path.join(__dirname, './client/dist/project')));
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.DB).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
-    );
-    var auth = require('./routes/auth');
-    const adUnitRoutes = require('./routes/adunit.route');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB).then(
+    () => {console.log('Database is connected') },
+err => { console.log('Can not connect to the database'+ err)}
+);
+var auth = require('./routes/auth');
+const adUnitRoutes = require('./routes/adunit.route');
 
-    app.use(passport.initialize());
-    app.use(bodyParser.json());
-    app.use(cors());
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(cors());
 
-    const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
-    app.use('/adunits', adUnitRoutes);
-    app.use('/auth', auth);
+app.use('/adunits', adUnitRoutes);
+app.use('/auth', auth);
 
-    const server = app.listen(port, function(){
-     console.log('Listening on port ' + port);
-    });
+const server = app.listen(port, function(){
+    console.log('Listening on port ' + port);
+});
+
+// socket io
+/*io.on('connection', function (socket) {
+  console.log('User connected');
+  socket.on('disconnect', function() {
+    console.log('User disconnected');
+  });
+  socket.on('save-message', function (data) {
+    console.log(data);
+    io.emit('new-message', { message: data });
+  });
+});*/
